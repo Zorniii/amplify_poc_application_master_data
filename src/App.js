540304e6dc7c4cf474b713b7d5140ee7258ee9bd
Zorniii <webmaster@zorniii.de>
2024-03-04 02:09:46 +0100

    Add AWS Amplify and basic login logout structure
    make sure to configure amplify to run this code successfully.
    
    There is a open to do for documentation in Readme.md

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import {Alert} from "react-bootstrap";
+import { Amplify } from 'aws-amplify';
 
-function App() {
-  return (
-    <div>
-      <Alert>This is a application</Alert>
-    </div>
-  );
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
+
+export default function App() {
+    return (
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
+    );
 }
-
-export default App;
